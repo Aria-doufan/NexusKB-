77ef9a45314d2180a42d6c22cf224b8d163bb08a
vintage9 <1552992250@qq.com>
2026-06-11 14:36:11 +0800

    Tighten EnterpriseRagGraph workflow wrapper

--- backend/app/rag/enterprise_rag_graph.py
+++ backend/app/rag/enterprise_rag_graph.py
@@ -14,8 +14,79 @@ class EnterpriseRagGraph:
             web_search_service=web_search_service,
         )
 
-    def __getattr__(self, name: str):
-        return getattr(self.workflow, name)
+    @property
+    def service(self):
+        return self.workflow.service
+
+    @property
+    def strategy_router(self):
+        return self.workflow.strategy_router
+
+    @property
+    def retrieval_pipeline(self):
+        return self.workflow.retrieval_pipeline
+
+    @property
+    def trace_store(self):
+        return self.workflow.trace_store
+
+    @property
+    def web_search_service(self):
+        return self.workflow.web_search_service
 
     async def run(self, state: RagState) -> RagResponse:
         return await self.workflow.run(state)
+
+    def initialize_trace(self, *args, **kwargs):
+        return self.workflow.initialize_trace(*args, **kwargs)
+
+    def planner(self, *args, **kwargs):
+        return self.workflow.planner(*args, **kwargs)
+
+    def strategy_select(self, *args, **kwargs):
+        return self.workflow.strategy_select(*args, **kwargs)
+
+    async def retrieve(self, *args, **kwargs):
+        return await self.workflow.retrieve(*args, **kwargs)
+
+    async def retrieve_decomposed(self, *args, **kwargs):
+        return await self.workflow.retrieve_decomposed(*args, **kwargs)
+
+    def evaluate_context(self, *args, **kwargs):
+        return self.workflow.evaluate_context(*args, **kwargs)
+
+    def decide_next_action(self, *args, **kwargs):
+        return self.workflow.decide_next_action(*args, **kwargs)
+
+    def rewrite_query(self, *args, **kwargs):
+        return self.workflow.rewrite_query(*args, **kwargs)
+
+    def expand_top_k(self, *args, **kwargs):
+        return self.workflow.expand_top_k(*args, **kwargs)
+
+    def decide_external_search(self, *args, **kwargs):
+        return self.workflow.decide_external_search(*args, **kwargs)
+
+    def decide_external_search_node(self, *args, **kwargs):
+        return self.workflow.decide_external_search_node(*args, **kwargs)
+
+    async def web_search_node(self, *args, **kwargs):
+        return await self.workflow.web_search_node(*args, **kwargs)
+
+    def merge_evidence_node(self, *args, **kwargs):
+        return self.workflow.merge_evidence_node(*args, **kwargs)
+
+    async def generate_answer(self, *args, **kwargs):
+        return await self.workflow.generate_answer(*args, **kwargs)
+
+    def build_insufficient_evidence(self, *args, **kwargs):
+        return self.workflow.build_insufficient_evidence(*args, **kwargs)
+
+    async def finalize_trace(self, *args, **kwargs):
+        return await self.workflow.finalize_trace(*args, **kwargs)
+
+    def _record_event(self, *args, **kwargs):
+        return self.workflow._record_event(*args, **kwargs)
+
+    def _now(self, *args, **kwargs):
+        return self.workflow._now(*args, **kwargs)
